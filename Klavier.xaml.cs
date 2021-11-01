ce36115cb3518587e66767193cb84944ea52c4c9
Lowly_TV <59661430+LowlyTV@users.noreply.github.com>
2021-11-01 12:04:33 +0100

    Eingabe mit Tastatur
    Das Klavier kann nun mit der Tastatur gespielt werden und gibt nun auch die Töne aus.

--- Klavier.xaml.cs
+++ Klavier.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Media;
 using System.Runtime;
+using System.Drawing;
 
 namespace SoundPlayer
 {
@@ -25,13 +26,101 @@ namespace SoundPlayer
         {
             InitializeComponent();
 
-            Environment.SpecialFolder mymusic = new Environment.SpecialFolder();
+            
 
-            String path = "/MusicNotes/C.wav";
+            
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) //C
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+
+
+            switch (e.Key)
+            {
+                case Key.A:
+                    System.Media.SoundPlayer soundC = new System.Media.SoundPlayer(SoundPlayer.Resource1.C);
+                    soundC.Play();
+                    break;
+                case Key.S:
+                    System.Media.SoundPlayer soundD = new System.Media.SoundPlayer(SoundPlayer.Resource1.D);
+                    soundD.Play();
+                    break;
+                case Key.D:
+                    System.Media.SoundPlayer soundE = new System.Media.SoundPlayer(SoundPlayer.Resource1.E);
+                    soundE.Play();
+                    break;
+                case Key.F:
+                    System.Media.SoundPlayer soundF = new System.Media.SoundPlayer(SoundPlayer.Resource1.F);
+                    soundF.Play();
+                    break;
+                case Key.G:
+                    System.Media.SoundPlayer soundG = new System.Media.SoundPlayer(SoundPlayer.Resource1.G);
+                    soundG.Play();
+                    break;
+                case Key.H:
+                    System.Media.SoundPlayer soundA = new System.Media.SoundPlayer(SoundPlayer.Resource1.A);
+                    soundA.Play();
+                    break;
+                case Key.J:
+                    System.Media.SoundPlayer soundB = new System.Media.SoundPlayer(SoundPlayer.Resource1.B);
+                    soundB.Play();
+                    break;
+                case Key.K:
+                    System.Media.SoundPlayer soundC1 = new System.Media.SoundPlayer(SoundPlayer.Resource1.C1);
+                    soundC1.Play();
+                    break;
+                case Key.L:
+                    System.Media.SoundPlayer soundD1 = new System.Media.SoundPlayer(SoundPlayer.Resource1.D1);
+                    soundD1.Play();
+                    break;
+                case Key.OemTilde:
+                    System.Media.SoundPlayer soundOe = new System.Media.SoundPlayer(SoundPlayer.Resource1.E1);
+                    soundOe.Play();
+                    break;
+                case Key.Oem7:
+                    System.Media.SoundPlayer soundF1 = new System.Media.SoundPlayer(SoundPlayer.Resource1.F1);
+                    soundF1.Play();
+                    break;
+                case Key.W:
+                    System.Media.SoundPlayer soundCis = new System.Media.SoundPlayer(SoundPlayer.Resource1.C_s);
+                    soundCis.Play();
+                    break;
+                case Key.E:
+                    System.Media.SoundPlayer soundDis = new System.Media.SoundPlayer(SoundPlayer.Resource1.D_s);
+                    soundDis.Play();
+                    break;
+                case Key.R:
+                    System.Media.SoundPlayer soundFis = new System.Media.SoundPlayer(SoundPlayer.Resource1.F_s);
+                    soundFis.Play();
+                    break;
+                case Key.T:
+                    System.Media.SoundPlayer soundGis = new System.Media.SoundPlayer(SoundPlayer.Resource1.G_s);
+                    soundGis.Play();
+                    break;
+                case Key.Z:
+                    System.Media.SoundPlayer soundB_oben = new System.Media.SoundPlayer(SoundPlayer.Resource1.B);
+                    soundB_oben.Play();
+                    break;
+                case Key.I:
+                    System.Media.SoundPlayer soundCis1 = new System.Media.SoundPlayer(SoundPlayer.Resource1.C_s1);
+                    soundCis1.Play();
+                    break;
+                case Key.O:
+                    System.Media.SoundPlayer soundDis1 = new System.Media.SoundPlayer(SoundPlayer.Resource1.D_s1);
+                    soundDis1.Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+        
+
+            private void Button_Click(object sender, RoutedEventArgs e) //C
         {
 
             System.Media.SoundPlayer sound = new System.Media.SoundPlayer(SoundPlayer.Resource1.C);
@@ -142,5 +231,8 @@ namespace SoundPlayer
             sound.Play();
 
         }
+
+       
+        
     }
 }
